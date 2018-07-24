c9441a4717fc0edc7a1b5aae1cfdb0a4ed5aeffe
long-march <36662196+long-march@users.noreply.github.com>
2018-07-24 08:47:54 +0100

    Automatic object creation, symbol table, object connections
    Template functions for creating audioobjects and adding them to a symbol table
    Function for connecting two audioobjects

--- src/Rumpetroll.h
+++ src/Rumpetroll.h
@@ -2,10 +2,11 @@
 
 #include <vector>
 #include <array>
+#include <map>
 
 #define BLOCKSIZE 64
 #define SAMPLE_RATE 44100
 #define PI 3.14159265
-#define TAU PI*2
+#define TAU  6.28318530
 
 typedef unsigned uint;
