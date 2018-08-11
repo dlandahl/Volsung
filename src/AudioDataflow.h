cbd6734f8c074ad4d3fe05324917b21b1c4d5bcb
long-march <36662196+long-march@users.noreply.github.com>
2018-08-11 01:29:23 +0100

    Update AudioDataflow.h

--- src/AudioDataflow.h
+++ src/AudioDataflow.h
@@ -2,12 +2,21 @@
 
 #include "Block.h"
 
+//An AudioConnector object is used to move data between inputs and outputs
+//An AudioConnector contains a buffer, which is a vector of Blocks.
+//Blocks are arrays of samples.
+
+//An AudioObject may write a block to an output, which will place it in all
+//	AudioConnectors that are connected. THE input that the Connector
+//	is also connected to can then read the block.
 class AudioConnector
 {
 public:
 	BlockBuffer                  buf;
 };
 
+//An input has a list of pointers to connectors that are connected, and a function
+//	for reading from their buffers.
 class AudioInput
 {
 public:
@@ -15,6 +24,9 @@ public:
 	Block                        read_block();
 };
 
+//An output is the opposite, it has a list of pointers to connectors, and a function
+//	for writing to them.
+//This way multiple connections can be on one input or output.
 class AudioOutput
 {
 public:
