afe74e8557ec93b388858afadaf027b3eec9c7d5
long-march <36662196+long-march@users.noreply.github.com>
2018-08-11 01:38:04 +0100

    Update FileoutObject.cpp

--- src/FileoutObject.cpp
+++ src/FileoutObject.cpp
@@ -8,7 +8,7 @@
 
 //The run function for this AudioObject is implemented to simply save
 //	data from its input to its data storage, which is later written
-//	to a file
+//	to a file.
 void FileoutObject::run()
 {
 	std::vector<Block> b = read_block();
@@ -18,7 +18,10 @@ void FileoutObject::run()
 	}
 }
 
+//This is the constructor for the object.
 FileoutObject::FileoutObject(std::string)
 {
+	//In the constructor, we push back the AudioInput array, to create a single input
+	//	This input will read the data that will be stored in a file
 	inputs.push_back({});
 }
