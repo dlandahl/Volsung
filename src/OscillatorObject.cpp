c0d63bc515e7aff2f5bf69c278026304238f0304
long-march <36662196+long-march@users.noreply.github.com>
2018-08-11 01:44:32 +0100

    Update OscillatorObject.cpp

--- src/OscillatorObject.cpp
+++ src/OscillatorObject.cpp
@@ -6,12 +6,21 @@
 #include "Block.h"
 #include "OscillatorObject.h"
 
+//The OscillatorObject IS an AudioObject which generates a sinewave and writes it
+//	to an AudioOutput.
+
 Block OscillatorObject::make_block()
 {
+	//We want to create a Block in order to write a sine wave to it.
+	//A Block is just a list of samples as floats.
 	Block b;
 
+	//If there is an AudioConnector to the input we create in the constructor below, we
+	//	will want to use the frequency from this input as the pitch.
 	if (inputs[0].connections.size())
 	{
+		//Read a block and write a sinewave of the frequency described in the block
+		//	to the block we created earlier.
 		Block p = read_block()[0];
 		for (uint i = 0; i < BLOCKSIZE; i++)
 		{
@@ -19,13 +28,17 @@ Block OscillatorObject::make_block()
 			phase = phase + (p[i] + 1) * 220 / SAMPLE_RATE;;
 			if (phase >= 1.0) { phase -= 1.0; }
 		}
+	//If there is no AudioConnector connected to the input:
 	} else {
 		for (uint i = 0; i < BLOCKSIZE; i++)
 		{
+			//Just fill the block with a sinewave at the frequency that is passed in as argument.
 			b[i] = sinf((i + block_index*BLOCKSIZE) * frequency);
 		}
 	}
 	block_index++;
+	
+	//Return the block.
 	return b;
 }
 
