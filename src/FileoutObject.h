6caaf980a0c0e12e398471736dbc0dc625f0fd5c
long-march <36662196+long-march@users.noreply.github.com>
2018-08-11 01:37:04 +0100

    Update FileoutObject.h

--- src/FileoutObject.h
+++ src/FileoutObject.h
@@ -2,7 +2,8 @@
 
 #include "AudioObject.h"
 
-//This object derives from AudioObject, and it stores data to a file
+//This object derives from AudioObject, and it stores 
+//	data from its single input to a file.
 class FileoutObject : public AudioObject
 {
 	void run() override;
