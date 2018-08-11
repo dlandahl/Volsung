4d9f4c157f1885ff401d51e018cdd1f588a6c2f9
long-march <36662196+long-march@users.noreply.github.com>
2018-08-11 01:24:51 +0100

    Update AudioObject.cpp

--- src/AudioObject.cpp
+++ src/AudioObject.cpp
@@ -3,18 +3,31 @@
 
 #include "AudioObject.h"
 
+//This is code in the AudioObject class.
+//Therefor, all AudioObjects such as oscillators and effects use this code,
+//	because they derive from the AudioObject class.
+//This code is used to read and write data to inputs and outputs.
+
+//To read a block:
 std::vector<Block> AudioObject::read_block()
 {
+	//Create a vector of blocks:
 	std::vector<Block> b;
+	
+	//From each input to the AudioObject, read a block into
+	//	one slot of the vector
 	for (uint i = 0; i < inputs.size(); i++) {
 		b.push_back(inputs[i].read_block());
 	}
+	
+	//Return the vector
 	return b;
+	
+	//This way, you may never read from just one particular input.
+	//Thereby, audio processing stays in sync.
 }
 
-//uint AudioObject::in_block_count() { return input.connection->get_block_count(); }
-//uint AudioObject::out_block_count() { return output.get_block_count(); }
-
+//To write a block to a particular output, just pass the block to that output.
 void AudioObject::write_block(Block b, uint output)
 {
 	outputs[output].write_block(b);
